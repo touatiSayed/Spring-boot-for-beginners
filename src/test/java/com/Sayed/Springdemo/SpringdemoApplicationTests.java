ef8544c138c5612a4623cc4f63b78102ab37b22b
touatisayed <touati.sayed47@gmail.com>
2025-02-27 14:46:25 +0100

    initializing spring boot project

---
+++ src/test/java/com/Sayed/Springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sayed.Springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
