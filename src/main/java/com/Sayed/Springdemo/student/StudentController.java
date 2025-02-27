f683cebc002daa873b86c608fc042942e70c62a7
touatisayed <touati.sayed47@gmail.com>
2025-02-27 19:51:22 +0100

    building the first sample API

---
+++ src/main/java/com/Sayed/Springdemo/student/StudentController.java
@@ -0,0 +1,21 @@
+package com.Sayed.Springdemo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+
+    @GetMapping
+    public List<String> findAllStudents() {
+        return List.of(
+                "Sayed",
+                "Hello world"
+        );
+    }
+
+}
